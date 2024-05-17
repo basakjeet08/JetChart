3a3add24cb07b3c0e1522ff8cf338c3280eee48d
Anirban Basak <jeetbasak2002@gmail.com>
2024-05-17 08:15:56 +0530

    docs - Refactored docs v1.

--- charts/src/main/java/dev/anirban/charts/circular/legend/GridLegendStrategy.kt
+++ charts/src/main/java/dev/anirban/charts/circular/legend/GridLegendStrategy.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.anirban.charts.circular.decoration.CircularDecoration
